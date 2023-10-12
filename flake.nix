701565f2e50db3554248e94cfcbca0552f9b5082
Haseeb Majid <hello@haseebmajid.dev>
2023-10-12 22:24:25 +0000

    feat: add pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -6,8 +6,9 @@
   inputs.gomod2nix.url = "github:nix-community/gomod2nix";
   inputs.gomod2nix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.gomod2nix.inputs.flake-utils.follows = "flake-utils";
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
 
-  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
+  outputs = inputs @ { self, nixpkgs, flake-utils, gomod2nix, pre-commit-hooks, ... }:
     (flake-utils.lib.eachDefaultSystem
       (system:
         let
@@ -23,6 +24,7 @@
           };
           devShells.default = callPackage ./shell.nix {
             inherit (gomod2nix.legacyPackages.${system}) mkGoEnv gomod2nix;
+            inherit pre-commit-hooks;
           };
         })
     );
--- shell.nix
+++ shell.nix
@@ -11,22 +11,32 @@
   )
 , mkGoEnv ? pkgs.mkGoEnv
 , gomod2nix ? pkgs.gomod2nix
+, pre-commit-hooks
+, ...
 }:
 
 let
   goEnv = mkGoEnv { pwd = ./.; };
+  pre-commit-check = pre-commit-hooks.lib.${pkgs.system}.run {
+    src = ./.;
+    hooks = {
+      gofmt.enable = true;
+    };
+  };
 in
+
 pkgs.mkShell {
+  inherit (pre-commit-check) shellHook;
   hardeningDisable = [ "all" ];
   packages = [
     goEnv
     gomod2nix
-    pkgs.gopls
     pkgs.golangci-lint
     pkgs.go_1_21
     pkgs.gotools
     pkgs.go-junit-report
     pkgs.go-task
     pkgs.delve
+
   ];
 }
