e8b0d11f57b8fa8564f41cc4021ba88d53cc3542
Haseeb Majid <hello@haseebmajid.dev>
2023-10-26 22:36:10 +0100

    chore: move formatting to golangci-lint

--- go.mod
+++ go.mod
@@ -26,6 +26,7 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	github.com/t-yuki/gocover-cobertura v0.0.0-20180217150009-aaee18c8195c // indirect
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- shell.nix
+++ shell.nix
@@ -20,39 +20,14 @@ let
   pre-commit-check = pre-commit-hooks.lib.${pkgs.system}.run {
     src = ./.;
     hooks = {
-      gofmt.enable = true;
       golangci-lint = {
         enable = true;
         name = "golangci-lint";
         description = "Lint my golang code";
         files = "\.go$";
-        entry = "${pkgs.golangci-lint}/bin/golangci-lint run --new-from-rev HEAD --fix";
+        entry = "task lint -- --new-from-rev HEAD --fix";
         require_serial = true;
         pass_filenames = false;
-      };
-      goimports = {
-        enable = true;
-        name = "goimports";
-        description = "Format my golang code";
-        files = "\.go$";
-        entry =
-          let
-            script = pkgs.writeShellScript "precommit-goimports" ''
-              set -e
-              failed=false
-              for file in "$@"; do
-                  # redirect stderr so that violations and summaries are properly interleaved.
-                  if ! ${pkgs.gotools}/bin/goimports -l -d "$file" 2>&1
-                  then
-                      failed=true
-                  fi
-              done
-              if [[ $failed == "true" ]]; then
-                  exit 1
-              fi
-            '';
-          in
-          builtins.toString script;
       };
     };
   };
